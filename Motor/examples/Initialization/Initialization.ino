89b66fb6724019087f53e72262e2e6e49bc63715
Manash Mandal <manashmndl@gmail.com>
2015-12-16 04:49:52 +0600

    Motor library

---
+++ Motor/examples/Initialization/Initialization.ino
@@ -0,0 +1,24 @@
+#include "Motor.h"
+
+motorpins pins = { 1,2, 3, 4 };
+
+Motor motor(pins);
+
+void setup()
+{
+
+	Serial.begin(9600);
+	motor.go(100, 200, FORWARD);
+	motor.go(255, 255, BACKWARD);
+	motor.print();
+	delete &motor;
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
